f532cea7d0502bb60f8e08d6f7795ecb68378186
elso.andras <elso.andras@bb819956-c407-11dd-8bad-cde060d8fd3c>
2009-12-05 18:30:52 +0000

    Encrypt passwords with TripleDES
    git-svn-id: http://transmission-remote-dotnet.googlecode.com/svn/trunk@376 bb819956-c407-11dd-8bad-cde060d8fd3c

--- TransmissionClientNew/Toolbox.cs
+++ TransmissionClientNew/Toolbox.cs
@@ -26,6 +26,7 @@ using System.IO;
 using System.Globalization;
 using System.Reflection;
 using System.Resources;
+using System.Security.Cryptography;
 
 namespace TransmissionRemoteDotnet
 {
@@ -277,6 +278,58 @@ namespace TransmissionRemoteDotnet
                 }
                 lv.ResumeLayout();
             }
+        }
+
+        static byte[] bytes = ASCIIEncoding.ASCII.GetBytes("TransmissionCool");
+
+        /// <summary>
+        /// Encrypt a string.
+        /// </summary>
+        /// <param name="originalString">The original string.</param>
+        /// <returns>The encrypted string.</returns>
+        /// <exception cref="ArgumentNullException">This exception will be 
+        /// thrown when the original string is null or empty.</exception>
+        public static string Encrypt(string originalString)
+        {
+            if (String.IsNullOrEmpty(originalString))
+            {
+                throw new ArgumentNullException
+                       ("The string which needs to be encrypted can not be null.");
+            }
+            TripleDESCryptoServiceProvider cryptoProvider = new TripleDESCryptoServiceProvider();
+            KeySizes[] aa= cryptoProvider.LegalKeySizes;
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateEncryptor(bytes, bytes), CryptoStreamMode.Write);
+            StreamWriter writer = new StreamWriter(cryptoStream);
+            writer.Write(originalString);
+            writer.Flush();
+            cryptoStream.FlushFinalBlock();
+            writer.Flush();
+            return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+        }
+
+        /// <summary>
+        /// Decrypt a crypted string.
+        /// </summary>
+        /// <param name="cryptedString">The crypted string.</param>
+        /// <returns>The decrypted string.</returns>
+        /// <exception cref="ArgumentNullException">This exception will be thrown 
+        /// when the crypted string is null or empty.</exception>
+        public static string Decrypt(string cryptedString)
+        {
+            if (String.IsNullOrEmpty(cryptedString))
+            {
+                throw new ArgumentNullException
+                   ("The string which needs to be decrypted can not be null.");
+            }
+            TripleDESCryptoServiceProvider cryptoProvider = new TripleDESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream
+                    (Convert.FromBase64String(cryptedString));
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+                cryptoProvider.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream);
+            return reader.ReadToEnd();
         }
     }
 }
