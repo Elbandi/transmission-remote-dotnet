61150a6c0330ba20871530bce893342caf21e85d
elso.andras <elso.andras@bb819956-c407-11dd-8bad-cde060d8fd3c>
2009-12-10 15:20:45 +0000

    Localize AboutDialog, sync AboutDialog and LocalSettingsDialog resx files
    git-svn-id: http://transmission-remote-dotnet.googlecode.com/svn/trunk@402 bb819956-c407-11dd-8bad-cde060d8fd3c

--- TransmissionClientNew/AboutDialog.Designer.cs
+++ TransmissionClientNew/AboutDialog.Designer.cs
@@ -64,9 +64,7 @@ namespace TransmissionRemoteDotnet
             // 
             // tableLayoutPanel
             // 
-            this.tableLayoutPanel.ColumnCount = 2;
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            resources.ApplyResources(this.tableLayoutPanel, "tableLayoutPanel");
             this.tableLayoutPanel.Controls.Add(this.labelDevelopers, 0, 2);
             this.tableLayoutPanel.Controls.Add(this.labelProductName, 0, 0);
             this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 0);
@@ -74,87 +72,42 @@ namespace TransmissionRemoteDotnet
             this.tableLayoutPanel.Controls.Add(this.labelHomepageLink, 0, 3);
             this.tableLayoutPanel.Controls.Add(this.panelDescription, 0, 4);
             this.tableLayoutPanel.Controls.Add(this.okButton, 0, 5);
-            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(164, 0);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
-            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(0, 6, 6, 6);
-            this.tableLayoutPanel.RowCount = 6;
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(430, 314);
-            this.tableLayoutPanel.TabIndex = 0;
             // 
             // labelDevelopers
             // 
             this.tableLayoutPanel.SetColumnSpan(this.labelDevelopers, 2);
-            this.labelDevelopers.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelDevelopers.LinkArea = new System.Windows.Forms.LinkArea(7, 0);
-            this.labelDevelopers.Location = new System.Drawing.Point(6, 52);
-            this.labelDevelopers.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            resources.ApplyResources(this.labelDevelopers, "labelDevelopers");
             this.labelDevelopers.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelDevelopers.Name = "labelDevelopers";
-            this.labelDevelopers.Size = new System.Drawing.Size(415, 17);
-            this.labelDevelopers.TabIndex = 33;
-            this.labelDevelopers.Text = "Developers:";
-            this.labelDevelopers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.labelDevelopers.UseCompatibleTextRendering = true;
             // 
             // labelProductName
             // 
-            this.labelProductName.AutoSize = true;
-            this.labelProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelProductName.Location = new System.Drawing.Point(6, 6);
-            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 0, 0);
+            resources.ApplyResources(this.labelProductName, "labelProductName");
             this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(106, 16);
-            this.labelProductName.TabIndex = 19;
-            this.labelProductName.Text = "Product Name";
-            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelVersion
             // 
-            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelVersion.Location = new System.Drawing.Point(206, 6);
-            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            resources.ApplyResources(this.labelVersion, "labelVersion");
             this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(215, 17);
-            this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "Version";
-            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCopyright
             // 
             this.tableLayoutPanel.SetColumnSpan(this.labelCopyright, 2);
-            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCopyright.Location = new System.Drawing.Point(6, 29);
-            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            resources.ApplyResources(this.labelCopyright, "labelCopyright");
             this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(415, 17);
-            this.labelCopyright.TabIndex = 21;
-            this.labelCopyright.Text = "Copyright";
-            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelHomepageLink
             // 
             this.tableLayoutPanel.SetColumnSpan(this.labelHomepageLink, 2);
-            this.labelHomepageLink.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelHomepageLink.LinkArea = new System.Windows.Forms.LinkArea(10, 51);
-            this.labelHomepageLink.Location = new System.Drawing.Point(6, 75);
-            this.labelHomepageLink.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            resources.ApplyResources(this.labelHomepageLink, "labelHomepageLink");
             this.labelHomepageLink.MaximumSize = new System.Drawing.Size(0, 17);
             this.labelHomepageLink.Name = "labelHomepageLink";
-            this.labelHomepageLink.Size = new System.Drawing.Size(415, 17);
-            this.labelHomepageLink.TabIndex = 22;
             this.labelHomepageLink.TabStop = true;
-            this.labelHomepageLink.Text = "Homepage: http://code.google.com/p/transmission-remote-dotnet";
-            this.labelHomepageLink.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.labelHomepageLink.UseCompatibleTextRendering = true;
             this.labelHomepageLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.labelHomepageLink_LinkClicked);
             // 
@@ -164,69 +117,44 @@ namespace TransmissionRemoteDotnet
             this.tableLayoutPanel.SetColumnSpan(this.panelDescription, 2);
             this.panelDescription.Controls.Add(this.textBoxDescription);
             this.panelDescription.Controls.Add(this.labelCredits);
-            this.panelDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDescription.Location = new System.Drawing.Point(3, 101);
+            resources.ApplyResources(this.panelDescription, "panelDescription");
             this.panelDescription.Name = "panelDescription";
-            this.panelDescription.Size = new System.Drawing.Size(418, 177);
-            this.panelDescription.TabIndex = 32;
             // 
             // textBoxDescription
             // 
             this.textBoxDescription.BackColor = System.Drawing.Color.White;
             this.textBoxDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxDescription.Location = new System.Drawing.Point(0, 13);
-            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
-            this.textBoxDescription.Multiline = true;
+            resources.ApplyResources(this.textBoxDescription, "textBoxDescription");
             this.textBoxDescription.Name = "textBoxDescription";
             this.textBoxDescription.ReadOnly = true;
-            this.textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxDescription.Size = new System.Drawing.Size(418, 164);
-            this.textBoxDescription.TabIndex = 32;
             this.textBoxDescription.TabStop = false;
-            this.textBoxDescription.Text = resources.GetString("textBoxDescription.Text");
-            this.textBoxDescription.WordWrap = false;
             // 
             // labelCredits
             // 
-            this.labelCredits.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelCredits.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.labelCredits, "labelCredits");
             this.labelCredits.Name = "labelCredits";
-            this.labelCredits.Size = new System.Drawing.Size(418, 13);
-            this.labelCredits.TabIndex = 31;
-            this.labelCredits.Text = "Credits && acknowledgements:";
             // 
             // okButton
             // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.okButton, "okButton");
             this.tableLayoutPanel.SetColumnSpan(this.okButton, 2);
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(346, 286);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(75, 19);
-            this.okButton.TabIndex = 24;
-            this.okButton.Text = "&OK";
             this.okButton.UseVisualStyleBackColor = true;
             // 
             // logoPictureBox
             // 
-            this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            resources.ApplyResources(this.logoPictureBox, "logoPictureBox");
             this.logoPictureBox.Image = global::TransmissionRemoteDotnet.Properties.Resources.welcomefinish;
-            this.logoPictureBox.Location = new System.Drawing.Point(0, 0);
             this.logoPictureBox.Name = "logoPictureBox";
-            this.logoPictureBox.Size = new System.Drawing.Size(164, 314);
-            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.logoPictureBox.TabIndex = 13;
             this.logoPictureBox.TabStop = false;
             // 
             // AboutDialog
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(594, 314);
             this.Controls.Add(this.tableLayoutPanel);
             this.Controls.Add(this.logoPictureBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -235,8 +163,6 @@ namespace TransmissionRemoteDotnet
             this.Name = "AboutDialog";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "AboutDialog";
             this.tableLayoutPanel.ResumeLayout(false);
             this.tableLayoutPanel.PerformLayout();
             this.panelDescription.ResumeLayout(false);
