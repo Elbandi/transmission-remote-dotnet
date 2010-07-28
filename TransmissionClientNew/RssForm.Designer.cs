e14fb9bf2394a540f7c06c20ee2f5b814154412c
elso.andras <elso.andras@bb819956-c407-11dd-8bad-cde060d8fd3c>
2010-07-28 16:16:22 +0000

    Add tooltips to buttons and localize rssform
    git-svn-id: http://transmission-remote-dotnet.googlecode.com/svn/trunk@658 bb819956-c407-11dd-8bad-cde060d8fd3c

--- TransmissionClientNew/RssForm.Designer.cs
+++ TransmissionClientNew/RssForm.Designer.cs
@@ -58,24 +58,19 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripProgressBar1,
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 388);
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(944, 22);
-            this.statusStrip1.TabIndex = 2;
-            this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripProgressBar1
             // 
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            resources.ApplyResources(this.toolStripProgressBar1, "toolStripProgressBar1");
             // 
             // toolStripStatusLabel1
             // 
-            this.toolStripStatusLabel1.AutoSize = false;
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(827, 17);
             this.toolStripStatusLabel1.Spring = true;
-            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // toolStrip1
             // 
@@ -86,63 +81,47 @@
             this.toolStripSeparator1,
             this.addTorrentButton,
             this.addTorrentWithOptionsButton});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(944, 39);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
             // 
             // RefreshButton
             // 
             this.RefreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.RefreshButton.Enabled = false;
+            resources.ApplyResources(this.RefreshButton, "RefreshButton");
             this.RefreshButton.Image = global::TransmissionRemoteDotnet.Properties.Resources.player_reload;
-            this.RefreshButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.RefreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.RefreshButton.Name = "RefreshButton";
-            this.RefreshButton.Size = new System.Drawing.Size(36, 36);
-            this.RefreshButton.Text = "toolStripButton1";
             this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
             // 
             // addTorrentButton
             // 
             this.addTorrentButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.addTorrentButton.Enabled = false;
+            resources.ApplyResources(this.addTorrentButton, "addTorrentButton");
             this.addTorrentButton.Image = global::TransmissionRemoteDotnet.Properties.Resources.edit_add;
-            this.addTorrentButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.addTorrentButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.addTorrentButton.Name = "addTorrentButton";
-            this.addTorrentButton.Size = new System.Drawing.Size(36, 36);
-            this.addTorrentButton.Text = "toolStripButton2";
             this.addTorrentButton.Click += new System.EventHandler(this.addTorrentButton_Click);
             // 
             // addTorrentWithOptionsButton
             // 
             this.addTorrentWithOptionsButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.addTorrentWithOptionsButton.Enabled = false;
+            resources.ApplyResources(this.addTorrentWithOptionsButton, "addTorrentWithOptionsButton");
             this.addTorrentWithOptionsButton.Image = global::TransmissionRemoteDotnet.Properties.Resources.net_add;
-            this.addTorrentWithOptionsButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
-            this.addTorrentWithOptionsButton.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.addTorrentWithOptionsButton.Name = "addTorrentWithOptionsButton";
-            this.addTorrentWithOptionsButton.Size = new System.Drawing.Size(36, 36);
-            this.addTorrentWithOptionsButton.Text = "toolStripButton3";
             this.addTorrentWithOptionsButton.Click += new System.EventHandler(this.addTorrentWithOptionsButton_Click);
             // 
             // FeedImageList
             // 
             this.FeedImageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
-            this.FeedImageList.ImageSize = new System.Drawing.Size(24, 24);
+            resources.ApplyResources(this.FeedImageList, "FeedImageList");
             this.FeedImageList.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 39);
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -152,24 +131,18 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.rssItemsListView);
-            this.splitContainer1.Size = new System.Drawing.Size(944, 349);
-            this.splitContainer1.SplitterDistance = 175;
-            this.splitContainer1.TabIndex = 1;
             // 
             // rssFeedsListView
             // 
-            this.rssFeedsListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            resources.ApplyResources(this.rssFeedsListView, "rssFeedsListView");
             this.rssFeedsListView.FullRowSelect = true;
             this.rssFeedsListView.HideSelection = false;
             this.rssFeedsListView.LargeImageList = this.FeedImageList;
-            this.rssFeedsListView.Location = new System.Drawing.Point(0, 0);
             this.rssFeedsListView.MultiSelect = false;
             this.rssFeedsListView.Name = "rssFeedsListView";
             this.rssFeedsListView.Scrollable = false;
             this.rssFeedsListView.ShowGroups = false;
-            this.rssFeedsListView.Size = new System.Drawing.Size(175, 349);
             this.rssFeedsListView.SmallImageList = this.FeedImageList;
-            this.rssFeedsListView.TabIndex = 0;
             this.rssFeedsListView.TileSize = new System.Drawing.Size(168, 24);
             this.rssFeedsListView.UseCompatibleStateImageBehavior = false;
             this.rssFeedsListView.View = System.Windows.Forms.View.Tile;
@@ -184,15 +157,12 @@
             this.categoryColumnHeader,
             this.descriptionColumnHeader,
             this.pubdateColumnHeader});
-            this.rssItemsListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            resources.ApplyResources(this.rssItemsListView, "rssItemsListView");
             this.rssItemsListView.FullRowSelect = true;
             this.rssItemsListView.GridLines = true;
             this.rssItemsListView.HideSelection = false;
-            this.rssItemsListView.Location = new System.Drawing.Point(0, 0);
             this.rssItemsListView.MultiSelect = false;
             this.rssItemsListView.Name = "rssItemsListView";
-            this.rssItemsListView.Size = new System.Drawing.Size(765, 349);
-            this.rssItemsListView.TabIndex = 0;
             this.rssItemsListView.UseCompatibleStateImageBehavior = false;
             this.rssItemsListView.View = System.Windows.Forms.View.Details;
             this.rssItemsListView.SelectedIndexChanged += new System.EventHandler(this.rssItemsListView_SelectedIndexChanged);
@@ -201,35 +171,28 @@
             // 
             // titleColumnHeader
             // 
-            this.titleColumnHeader.Text = "Title";
-            this.titleColumnHeader.Width = 350;
+            resources.ApplyResources(this.titleColumnHeader, "titleColumnHeader");
             // 
             // categoryColumnHeader
             // 
-            this.categoryColumnHeader.Text = "Category";
-            this.categoryColumnHeader.Width = 150;
+            resources.ApplyResources(this.categoryColumnHeader, "categoryColumnHeader");
             // 
             // descriptionColumnHeader
             // 
-            this.descriptionColumnHeader.Text = "Description";
-            this.descriptionColumnHeader.Width = 250;
+            resources.ApplyResources(this.descriptionColumnHeader, "descriptionColumnHeader");
             // 
             // pubdateColumnHeader
             // 
-            this.pubdateColumnHeader.Text = "Pub Date";
-            this.pubdateColumnHeader.Width = 150;
+            resources.ApplyResources(this.pubdateColumnHeader, "pubdateColumnHeader");
             // 
             // RssForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 410);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RssForm";
-            this.Text = "Rss Downloader";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
